8f3172b2d62ffc928d76e0231114f810a9c8ee00
unniznd <au6695@gmail.com>
2022-03-25 09:28:54 +0530

    urls added

--- server/urls.py
+++ server/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 from onepassword import views as onepasswordviews
-
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',onepasswordviews.main,name='onepassword'),
-    path('howitworks/',onepasswordviews.howitworks,name='howitworks')
+    path('howitworks/',onepasswordviews.howitworks,name='howitworks'),
+    path('sitemap.xml'),
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 handler404 = onepasswordviews.page_not_found
 handler500 = onepasswordviews.internal_error
