0a81ed22f590fd8874239baa12c23dc2805bfb8a
unniznd <au6695@gmail.com>
2022-03-25 09:31:55 +0530

    changes

--- server/urls.py
+++ server/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',onepasswordviews.main,name='onepassword'),
     path('howitworks/',onepasswordviews.howitworks,name='howitworks'),
-    
-    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
 handler404 = onepasswordviews.page_not_found
 handler500 = onepasswordviews.internal_error
